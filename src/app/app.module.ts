b5b99211bd2c2c5d0c7317d2baf53af7fe5209ea
jefersson00 <jeferssonserrano00@gmail.com>
2020-09-25 10:01:47 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarHomeComponent } from './toolbar-home/toolbar-home.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ToolbarLoginComponent } from './toolbar-login/toolbar-login.component';
+import { FormLoginComponent } from './form-login/form-login.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    ToolbarLoginComponent,
+    FormLoginComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
